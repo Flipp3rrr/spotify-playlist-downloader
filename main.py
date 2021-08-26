0f1d4cc48bb1b62bd17580057863e880ffcb4bee
Flipp3rrr <flipp3rrr@protonmail.com>
2021-08-26 14:24:36 +0200

    clean up code, better loggin, error handling, code comments, etc

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-# Import Spotipy to use Spotify features
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-# Import urllib.request to search YouTube
+try:
+    # Import Spotipy to get the track list from the playlist and track information
+    import spotipy
+    from spotipy.oauth2 import SpotifyClientCredentials
+    # Import youtube_dl to download YouTube videos
+    import youtube_dl
+except Exception as error:
+    raise Exception("Failed to import either 'spotipy' or 'youtube_dl', {error}".format(error=error))
+# Import urllib.request which will later be used to search YouTube videos
 import urllib.request
-# Import youtube_dl to download YouTube videos
-import youtube_dl
-# Import os to save data to files
+# Import os and shutil to do things with files and directories
 import os
+import shutil
 # Import logging
 import logging
-# Import JSON to dump data to a JSON file
+# Import JSON to dump data to a JSON file, it's not actually needed but it's nice to be able
+# to see the information I got from Spotify
 import json
-# Import time to sleep
+# Import time to wait for a short time so Spotify and YouTube won't think it's a DOS attack
 import time
 # Import SSL just so I can bypass SSL verification later
 import ssl
-# Regex yeet
+# Import regex
 import re
-# Shutil to yeet things into place
-import shutil
 
 # Check for log file, delete it if it exists
 if os.path.exists("spotify-playlist-downloader.log"):
@@ -40,7 +43,7 @@ if not os.path.exists(outDir):
 
 # Function to retrieve data from a file, or ask for the information to then put in a file
 def retrieveFromFile(fileName, purpose):
-    # Check for and then read data from file
+    # Check for file and then read data from file
     if os.path.exists(fileName):
         logging.info("Checked for '{file}' and it exists".format(file=fileName))
         with open(fileName, "r") as file:
@@ -67,35 +70,13 @@ def deleteFile(fileName):
         os.remove(fileName)
         logging.info("Deleted '{file}'".format(file=fileName))
 
-# Function to delete all files with 'ext' file extension, if any exist
-def deleteFileExt(ext):
-    # List all files in the current directory
-    files = os.listdir(runDir)
+# Function to delete all files with 'ext' file extension in the 'directory' directory, if any exist
+def deleteFileExt(ext, directory):
     # Check every file, delete it if it has 'ext' extension
-    for item in files:
+    for item in os.listdir(directory):
         if item.endswith(ext):
-            os.remove(os.path.join(runDir, item))
+            os.remove(os.path.join(directory, item))
             logging.info("Checked for '{ext}' and '{file}' exists, deleted".format(ext=ext, file=item))
-
-# User options and input
-print("""
-SPOTIFY-PLAYLIST-DOWNLOADER
-(get you client ID and secret from 'developer.spotify.com')
-
-[1] Download playlists
-[2] Delete data
-""")
-choice = input(">> ")
-
-# Choice '1', download playlist
-if choice == "1":
-    # Get ID and secret
-    clientID = retrieveFromFile("clientID.secret", "Spotify client ID")
-    spotifySecret = retrieveFromFile("spotifySecret.secret", "Spotify secret")
-
-    # ID and secret to SpotifyClientCredentials
-    clientCreds = SpotifyClientCredentials(clientID, spotifySecret)
-    sp = spotipy.Spotify(client_credentials_manager=clientCreds)
 
 # Function to retrieve track IDs from playlist
 def retrieveTrackIDs(ID):
@@ -109,23 +90,45 @@ def retrieveTrackIDs(ID):
 # Function to retrieve details from tracks
 def retrieveTrackData(trackID):
     meta = sp.track(trackID)
+    # I only need the name of the track and the artist, get it here
     trackDetails = {"name": meta["name"], "artist": meta["album"]["artists"][0]["name"]}
     return trackDetails
 
-# Continue choice '1', download playlist
+# User options and input
+print("""
+SPOTIFY-PLAYLIST-DOWNLOADER
+(get you client ID and secret from 'developer.spotify.com')
+
+[1] Download playlists
+[2] Delete data (client ID, secret, logs, output, etc.)
+""")
+choice = input(">> ")
+
+# Choice '1', download playlist
 if choice == "1":
+    # Get ID and secret
+    clientID = retrieveFromFile("clientID.secret", "Spotify client ID")
+    spotifySecret = retrieveFromFile("spotifySecret.secret", "Spotify secret")
+
+    # ID and secret to SpotifyClientCredentials
+    clientCreds = SpotifyClientCredentials(clientID, spotifySecret)
+    sp = spotipy.Spotify(client_credentials_manager=clientCreds)
+
+    # Ask for the URL to the playlist
     playlistURL = input("Playlist URL: ")
-    # playlist URL = https://open.spotify.com/playlist/<ID>?si=<junk>
+    # Playlist URL = https://open.spotify.com/playlist/<ID>?si=<junk>
     #                (34)                              (ID)(20)
     # Now remove the first 34 and last 20 characters to get the playlist ID
-    playlistID = playlistURL[34:-20]
+    playlistID = playlistURL[34:-20] # Yeah hardcoding this is a bad idea, need to fix it sometime
 
+    # Get all the track IDs
     trackIDs = retrieveTrackIDs(playlistID)
     print("There are {songs} songs in this playlist".format(songs = len(trackIDs)))
     logging.debug("Got the following IDs from playlist {id}: {tracks}".format(id=playlistID, tracks=trackIDs))
 
     tracks = []
     for a in range(len(trackIDs)):
+        # Wait for a tiny while just so it will look less like a DOS
         time.sleep(.1)
         track = retrieveTrackData(trackIDs[a])
         tracks.append(track)
@@ -146,11 +149,12 @@ if choice == "1":
     # Delete directory contents if it exists
     if os.path.exists(playlistDir):
         shutil.rmtree(playlistDir)
+        logging.info("Checked for '{dir}' and '{dir}' exists, deleted".format(dir=playlistDir))
 
-    # Create directory if it doesn't exist
-    if not os.path.exists(playlistDir):
-        os.makedirs(playlistDir)
+    # Create directory
+    os.makedirs(playlistDir)
 
+    # Go through the playlist and download each track
     for b in range(len(tracks)):
         # Create search query
         currentTrack = tracks[b]
@@ -158,11 +162,12 @@ if choice == "1":
         currentTrackArtist = currentTrack["artist"]
         searchUnsan = currentTrackName + " " + currentTrackArtist
 
-        # I need only alphanumeric characters or urllib will complain
+        # I need only alphanumeric characters or 'urllib' will complain,
+        # TODO: convert characters to closest ascii character
         search = re.sub("[^A-Za-z0-9]+", " ", searchUnsan)
         logging.info("Changed '{search1}' to '{search2}'".format(search1=searchUnsan, search2=search))
 
-        # Fuck you SSL
+        # Create 'ctx' context to bypass SSL when searching with 'urllib'
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE 
@@ -170,7 +175,7 @@ if choice == "1":
         # Create URL from query
         youtubeURL = "http://www.youtube.com/results?search_query=" + search.replace(" ", "%20")
 
-        # Results
+        # Make request with 'youtubeURL' and output results to 'result'
         logging.info("Getting results from {url}".format(url=youtubeURL))
         result = urllib.request.urlopen(youtubeURL, context=ctx)
 
@@ -180,41 +185,55 @@ if choice == "1":
         print("Downloading '{track}' from '{url}'".format(track=currentTrackName, url=downloadURL))
         logging.info("Downloading '{track}' from '{url}'".format(track=currentTrackName, url=downloadURL))
 
+        # Wait for a tiny while just so it will look less like a DOS
         time.sleep(.1)
 
-        # Download...
-        try:
+        # Download
+        try: # Internet connection can be weird so let's do some error handling
+            # Disable SSL by using 'noSSL' with youtube_dl
             noSSL = {
                 "nocheckcertificate": True,
             }
             videoInfo = youtube_dl.YoutubeDL(noSSL).extract_info(url=downloadURL, download=False)
             filename = f"{videoInfo['title']}.mp3"
             filePath = os.path.join(runDir, filename)
+            
+            # Options for the download
             options = {
                 "format": "bestaudio/best",
                 "keepvideo": False,
                 "outtmpl": filename,
-                # And again, fuck you SSL
+                # Again disable SSL
                 "nocheckcertificate": True,
             }
+
+            # Actual download
             with youtube_dl.YoutubeDL(options) as ydl:
                 ydl.download([videoInfo['webpage_url']])
-        except Exception as error:
+
+        except Exception as error: # Most likely this exception would be because the video is age restriction
             logging.warning("Did not download {track} because of {error}".format(track=currentTrackName, error=error))
             print("Did not download {track} because of {error}".format(track=currentTrackName, error=error))
 
-        try:
-            # Move to 'playlistDir'
-            filePlace = os.path.join(playlistDir, filename)
-            shutil.move(filePath, playlistDir)
-            print("Downloaded {track} and moved to {location}".format(track=currentTrackName, location=filePlace))
-        except Exception as error:
-            logging.warning("Couldn't move {file} because {error}".format(file=filename, error=error))
+        else: # Only move the file if the download actually was done
+            try:
+                # Move to 'playlistDir'
+                filePlace = os.path.join(playlistDir, filename)
+                shutil.move(filePath, playlistDir)
+                print("Downloaded {track} and moved to {location}".format(track=currentTrackName, location=filePlace))
+            except Exception as error:
+                logging.warning("Couldn't move {file} because {error}".format(file=filename, error=error))
 
 # Choice 2, delete data
 elif choice == "2":
-    deleteFileExt(".secret")
-    deleteFileExt(".json")
+    # Delete files that end up in the directory containing 'main.py'
+    deleteFileExt(".log", runDir)
+    deleteFileExt(".secret", runDir)
+    deleteFileExt(".json", runDir)
+    deleteFileExt(".mp3", runDir)
+    # Delete everything in the output directory and the directory itself
+    if os.path.exists(outDir):
+        shutil.rmtree(outDir)
 
 # Backup in case of ID10T error
 else:
